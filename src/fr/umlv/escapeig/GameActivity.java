3c601ad99c14a79c8bacb6d6f5eb644f5ae6974c
Louis <louis@pc19.home>
2013-05-26 20:46:55 +0200

    Choose level before playing

--- src/fr/umlv/escapeig/GameActivity.java
+++ src/fr/umlv/escapeig/GameActivity.java
@@ -1,19 +1,23 @@
 package fr.umlv.escapeig;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.gesture.GestureOverlayView;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+import fr.umlv.escapeig.builder.BuilderWorld;
 import fr.umlv.escapeig.gesture.GestureHandler;
 import fr.umlv.escapeig.view.BoardView;
 import fr.umlv.escapeig.view.GestureView;
 import fr.umlv.escapeig.world.Board;
 
 public class GameActivity extends Activity {
-	
+
 	Board board;
 	BoardView boardView;
 	GestureOverlayView mView;
@@ -25,20 +29,42 @@ public class GameActivity extends Activity {
 		//Fullscreen
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-							 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		GestureHandler.self.loadGesture(this, R.raw.gestures);
+
+		System.out.println("GAME ACTIVITY");
 		
-		board = Board.createBoard(this, LA STRING DE L\'INTENT);
-		
+		Intent intent = new Intent(this, ImportLevelListActivity.class);
+		startActivityForResult(intent, 1);
+	}
+
+	private void initiate(String path) {
+		board = Board.createBoard(this, path);
+
 		boardView = new BoardView(this, board);
-		
-        mView = new GestureView(this);
-        mView.setGestureVisible(false);
-        mView.addOnGesturePerformedListener(GestureHandler.self);
-        mView.addView(boardView);
-        setContentView(mView);
-        
+
+		mView = new GestureView(this);
+		mView.setGestureVisible(false);
+		mView.addOnGesturePerformedListener(GestureHandler.self);
+		mView.addView(boardView);
+		setContentView(mView);
+
 		board.play();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (requestCode == 1) { // Activity for add ship
+			if(resultCode == RESULT_OK){
+				String result = data.getStringExtra("result");
+				initiate(result);
+			}
+			else {
+				// probleme result result code ou valeurs x et y
+				return;
+			}
+		} 
 	}
 }
